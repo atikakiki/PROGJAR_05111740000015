8e704543029fcfbfca56f506440537e6775c6541
atikakiki <atikarizki41@gmail.com>
2020-04-06 13:47:48 +0700

    upload_repo

--- tugas5/server_thread_chat.py
+++ tugas5/server_thread_chat.py
@@ -1,26 +1,35 @@
 from socket import *
 import socket
 import threading
-import thread
 import time
 import sys
 import json
-from http import HttpServer
+import logging
+from chat import Chat
 
-httpserver = HttpServer()
+chatserver = Chat()
 
 class ProcessTheClient(threading.Thread):
-	def __init__(self,connection,address):
+	def __init__(self, connection, address):
 		self.connection = connection
 		self.address = address
 		threading.Thread.__init__(self)
 
 	def run(self):
+		rcv=""
 		while True:
-			data = self.connection.recv(1024)
+			data = self.connection.recv(32)
 			if data:
-				self.connection.sendall("{}" . format(httpserver.proses(data)))
-                                break
+				d = data.decode()
+				rcv=rcv+d
+				if rcv[-2:]=='\r\n':
+					#end of command, proses string
+					logging.warning("data dari client: {}" . format(rcv))
+					hasil = json.dumps(chatserver.proses(rcv))
+					hasil=hasil+"\r\n\r\n"
+					logging.warning("balas ke  client: {}" . format(hasil))
+					self.connection.sendall(hasil.encode())
+					rcv=""
 			else:
 				break
 		self.connection.close()
@@ -29,6 +38,7 @@ class Server(threading.Thread):
 	def __init__(self):
 		self.the_clients = []
 		self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		threading.Thread.__init__(self)
 
 	def run(self):
@@ -36,7 +46,7 @@ class Server(threading.Thread):
 		self.my_socket.listen(1)
 		while True:
 			self.connection, self.client_address = self.my_socket.accept()
-			print >> sys.stderr, 'connection from', self.client_address
+			logging.warning("connection from {}" . format(self.client_address))
 			
 			clt = ProcessTheClient(self.connection, self.client_address)
 			clt.start()
